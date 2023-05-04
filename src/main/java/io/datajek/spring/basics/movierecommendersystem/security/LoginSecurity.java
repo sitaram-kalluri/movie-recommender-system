64a65f580318dabf468135789740c6f7f9c43e85
Sitaram Kalluri <kalluriramkumar@gmail.com>
2023-05-04 08:48:04 +0530

    Modify spring security with custom username and password

--- src/main/java/io/datajek/spring/basics/movierecommendersystem/security/LoginSecurity.java
+++ src/main/java/io/datajek/spring/basics/movierecommendersystem/security/LoginSecurity.java
@@ -1,20 +1,39 @@
 package io.datajek.spring.basics.movierecommendersystem.security;
 
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-public class LoginSecurity {
+import java.util.ArrayList;
 
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.
-                authorizeHttpRequests().
-                anyRequest().
-                authenticated().
-                and().
-                httpBasic().
-                and().build();
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+class LoginSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        UserDetails user = User.builder().username("user").password("password").passwordEncoder(encoder::encode).authorities(new ArrayList<>()).build();
+        return new InMemoryUserDetailsManager(user);
+    }
 
 }
